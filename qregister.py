8dadffbe0bfd69559c6039dd984f25f3a7fdcd57
Rolin Blake <rolinmblake@gmail.com>
2023-07-07 01:30:57 -0500

    removed tensor product as was un-necessary

--- qregister.py
+++ qregister.py
@@ -11,19 +11,12 @@ class qRegister:
         if validateRegister(qbits):
             raise Exception(f"-> Qubits not valid to form a Quantum Register.")
         self.qubits = qbits
-        self.probs = []
-        # Find probability amplitudes of each qubit using tensor product
-        for q in self.qubits:
-            factors = []
-            for p in self.qubits:
-                if q is p:
-                    continue
-                else:
-                    factors.append(np.array(p.c0, p.c1))
-            self.probs.append(np.tensordot(np.array(q.c0, q.c1), np.array(factors)))
         self.n_qubits = len(qbits)
+        self.n_states = 2**self.n_qubits
 
 if __name__ == "__main__":
     register = qRegister([qubit(1/np.sqrt(2), -1/np.sqrt(2)), qubit(-1/np.sqrt(2), 1/np.sqrt(2))])
     for i in range(0, len(register.qubits)):
-        print(f"Qubit: {register.qubits[i]} Amplitude: {register.probs[i]}")
+        prob0 = round(abs(register.qubits[i].c0)**2, 2)
+        prob1 = round(abs(register.qubits[i].c1)**2, 2)
+        print(f"Qubit: {register.qubits[i]} Amplitudes: (0) {prob0} (1): {prob1}")
